1a770b35b87ece1a3d993ab18dd9124474dcaab3
Kier Heyl <kierheyl@gmail.com>
2021-04-17 23:30:14 -0500

    Fixed the data providers for modded blocks and items. Optimized the code with some additional methods to avoid redundency.

--- src/main/java/space/gamr/villages_overhauled/data/client/ModBlockStateProvider.java
+++ src/main/java/space/gamr/villages_overhauled/data/client/ModBlockStateProvider.java
@@ -1,9 +1,14 @@
 package space.gamr.villages_overhauled.data.client;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.fml.RegistryObject;
 import space.gamr.villages_overhauled.VillagesOverhauled;
 import space.gamr.villages_overhauled.setup.ModBlocks;
 
@@ -13,18 +18,42 @@ public class ModBlockStateProvider extends BlockStateProvider {
         super(gen, VillagesOverhauled.MOD_ID, exFileHelper);
     }
 
+    private ResourceLocation mcBlockLoc(Block block) {
+        return mcLoc(ModelProvider.BLOCK_FOLDER + "/" + block.getRegistryName().getPath());
+    }
+
+    private ResourceLocation modBlockLoc(RegistryObject<Block> block, String suffix) {
+        return modLoc(ModelProvider.BLOCK_FOLDER + "/" + modBlockPath(block) + suffix);
+    }
+
+    private String modBlockPath(RegistryObject<Block> block) {
+        return block.get().getRegistryName().getPath();
+    }
+
     @Override
     protected void registerStatesAndModels() {
         simpleBlock(ModBlocks.COBBLED_SANDSTONE.get());
-        String sandPathPath = ModBlocks.SAND_PATH.getId().getPath();
+
         simpleBlock(
                 ModBlocks.SAND_PATH.get(),
-                models().cubeBottomTop(
-                        sandPathPath,
-                        new ResourceLocation(VillagesOverhauled.MOD_ID, "block/" + sandPathPath + "_side"),
-                        new ResourceLocation(VillagesOverhauled.MOD_ID, "block/" + sandPathPath + "_bottom"),
-                        new ResourceLocation(VillagesOverhauled.MOD_ID, "block/" + sandPathPath + "_top")
-                )
+                models().getBuilder(modBlockPath(ModBlocks.SAND_PATH))
+                        .parent(models().getExistingFile(mcLoc(ModelProvider.BLOCK_FOLDER + "/" + ModelProvider.BLOCK_FOLDER)))
+                        .texture("side", modBlockLoc(ModBlocks.SAND_PATH, "_side"))
+                        .texture("top", modBlockLoc(ModBlocks.SAND_PATH, "_top"))
+                        .texture("bottom", mcBlockLoc(Blocks.SAND))
+                        .texture("particle", mcBlockLoc(Blocks.SAND))
+                        .element()
+                        .from(0.0F,0.0F,0.0F)
+                        .to(16.0F, 15.0F,16.0F)
+                        .face(Direction.NORTH).end()
+                        .face(Direction.SOUTH).end()
+                        .face(Direction.WEST).end()
+                        .face(Direction.EAST).end()
+                        .faces((dir, face) -> face.texture("#side").cullface(dir))
+                        .face(Direction.DOWN).texture("#bottom").cullface(Direction.DOWN).end()
+                        .face(Direction.UP).texture("#top").end()
+                        .end()
         );
     }
 }
+
--- src/main/java/space/gamr/villages_overhauled/data/client/ModItemModelProvider.java
+++ src/main/java/space/gamr/villages_overhauled/data/client/ModItemModelProvider.java
@@ -1,8 +1,11 @@
 package space.gamr.villages_overhauled.data.client;
 
+import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.fml.RegistryObject;
 import space.gamr.villages_overhauled.VillagesOverhauled;
 import space.gamr.villages_overhauled.setup.ModBlocks;
 
@@ -11,14 +14,15 @@ public class ModItemModelProvider extends ItemModelProvider {
         super(generator, VillagesOverhauled.MOD_ID, existingFileHelper);
     }
 
-    private void registerBlockModel(String name) {
-        withExistingParent(name, modLoc("block/" + name));
+    private void registerBlockModel(RegistryObject<Block> block) {
+        String path = block.getId().getPath();
+        withExistingParent(path, modLoc(ModelProvider.BLOCK_FOLDER + "/" + path));
     }
 
     @Override
     protected void registerModels() {
-        registerBlockModel(ModBlocks.COBBLED_SANDSTONE.getId().getPath());
-        registerBlockModel(ModBlocks.SAND_PATH.getId().getPath());
+        registerBlockModel(ModBlocks.COBBLED_SANDSTONE);
+        registerBlockModel(ModBlocks.SAND_PATH);
     }
 
 }
